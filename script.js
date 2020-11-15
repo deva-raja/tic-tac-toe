b34997829c8c06397b0206a90398d02e2ba35d88
Vinu Devaraj <vinudevaraj007@gmail.com>
2020-11-15 22:34:10 +0530

    add modules and factories

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
-let arrayMoves = ["", "", "", "", "", "", "", "", ""];
+// gameboard module displays playground board
 let gameboard = (function () {
   let container = document.querySelector("#container");
-  //   prettier-ignore
-  function display(){
-  container.innerHTML = arrayMoves.map((item,i)=>{
-      return` <div data-index=${i} class="blocks  ${(item==="x"||item==="0")?"clicked":""}">${item}</div> `;
-  }).join("");
-}
-  return { display };
-})();
+  let arrayMoves = ["", "", "", "", "", "", "", "", ""];
 
+  function display() {
+    container.innerHTML = arrayMoves
+      .map((item, i) => {
+        return ` <div data-index=${i} class="blocks  ${item === "x" || item === "0" ? "clicked" : ""}">${item}</div> `;
+      })
+      .join("");
+  }
+  return { display, arrayMoves };
+})();
 gameboard.display();
 
-function move(e) {
-  let el = e.target;
-  if (el.matches(".clicked")) return;
-  let index = el.dataset.index;
+// gameTickLogic module for x and 0's
+let gameTickLogic = (function () {
+  function playerMove(e) {
+    let result = document.querySelector("#result");
+    let el = e.target;
+    if (el.matches(".clicked")) return;
+    let index = el.dataset.index;
 
-  function subToggle(symbol) {
-    symbolName = symbol;
-    arrayMoves.splice(index, 1, symbolName);
-    gameboard.display();
-    return win();
+    function subToggle(symbol) {
+      symbolName = symbol;
+      gameboard.arrayMoves.splice(index, 1, symbolName);
+      gameboard.display();
+      let gameResult = gameWinLogic.win();
+      result.textContent = gameResult;
+    }
+
+    if (this.parentNode.matches(".player1")) {
+      this.parentNode.classList.toggle("player1");
+      return subToggle("x");
+    }
+    if (!this.parentNode.matches(".player1")) {
+      this.parentNode.classList.toggle("player1");
+      return subToggle("0");
+    }
   }
 
-  if (this.parentNode.matches(".player1")) {
-    this.parentNode.classList.toggle("player1");
-    return subToggle("x");
+  function tick() {
+    let boards = document.querySelector("#container");
+    boards.addEventListener("click", playerMove);
   }
-  if (!this.parentNode.matches(".player1")) {
-    this.parentNode.classList.toggle("player1");
-    return subToggle("0");
+  return { tick };
+})();
+gameTickLogic.tick();
+
+// gameWinLogic module for checking win or draw
+let gameWinLogic = (function () {
+  function win() {
+    function winTester(i, j, k) {
+      if (gameboard.arrayMoves[i] === "" || gameboard.arrayMoves[j] === "" || gameboard.arrayMoves[k] === "") return;
+      if (gameboard.arrayMoves[i] === gameboard.arrayMoves[j] && gameboard.arrayMoves[j] === gameboard.arrayMoves[k]) return "win";
+    }
+    if (winTester(0, 1, 2) === "win") return "win";
+    if (winTester(3, 4, 5) === "win") return "win";
+    if (winTester(6, 7, 8) === "win") return "win";
+    if (winTester(0, 3, 6) === "win") return "win";
+    if (winTester(1, 4, 7) === "win") return "win";
+    if (winTester(2, 5, 8) === "win") return "win";
+    if (winTester(0, 4, 8) === "win") return "win";
+    if (winTester(2, 4, 6) === "win") return "win";
+    if (!gameboard.arrayMoves.some((x) => x === "")) return "draw";
   }
+  return { win };
+})();
+
+// player factory
+function player(name) {
+  return { name };
 }
 
-function tick() {
-  let boards = document.querySelector("#container");
-  boards.addEventListener("click", move);
-}
-tick();
-
-function win() {
-  if(arrayMoves.some(x=>x==="")) return;
-  if (arrayMoves[0] === arrayMoves[1] && arrayMoves[1] === arrayMoves[2]) console.log("win");
-  if (arrayMoves[3] === arrayMoves[4] && arrayMoves[4] === arrayMoves[5]) console.log("win");
-  if (arrayMoves[6] === arrayMoves[7] && arrayMoves[7] === arrayMoves[8]) console.log("win");
-  if (arrayMoves[0] === arrayMoves[3] && arrayMoves[3] === arrayMoves[6]) console.log("win");
-  if (arrayMoves[1] === arrayMoves[4] && arrayMoves[4] === arrayMoves[7]) console.log("win");
-  if (arrayMoves[2] === arrayMoves[5] && arrayMoves[5] === arrayMoves[6]) console.log("win");
-  if (arrayMoves[0] === arrayMoves[4] && arrayMoves[4] === arrayMoves[6]) console.log("win");
-  if (arrayMoves[2] === arrayMoves[4] && arrayMoves[4] === arrayMoves[6]) console.log("win");
-}
